def7bb40f67356f21e50c8547ca98803d0425553
moha-test-github <139517477+moha-test-github@users.noreply.github.com>
2023-10-09 17:36:07 +0300

    Update script.js
    2k

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 const account1 = {
   // owner: 'Jonas Schmedtmann',
   owner: 'shire mohamed ibrahim',
-  movements: [9100, -450, -350, 5000, -650, 4300,7330, 17170,-250, 7300,-500,650,120,-150],
+  movements: [-150,9100, -450, -350, 5000, -650, 4300,7330, 17170,-250, 7300,-500,650,120,-150,2000],
   interestRate: 1.2, // %
   pin: 1111,
 };
